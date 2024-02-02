69fce096be7b0f1d6343b2ff10dc76d70f6983b5
Aitishnitsa <aitishnitsa@gmail.com>
2024-02-02 19:55:07 +0200

    dropdown item

--- dist/script.js
+++ dist/script.js
@@ -4,9 +4,9 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 document.addEventListener('DOMContentLoaded', function () {
     // Function to toggle mobile menu
     function toggleMobileMenu() {
-        let mobileMenu = document.getElementById('mobile-menu');
-        let closedPath = document.querySelector('.closed-path');
-        let openPath = document.querySelector('.open-path');
+        const mobileMenu = document.getElementById('mobile-menu');
+        const closedPath = document.querySelector('.closed-path');
+        const openPath = document.querySelector('.open-path');
 
         mobileMenu.classList.toggle('hidden');
         closedPath.style.display = mobileMenu.classList.contains('hidden') ? 'block' : 'none';
@@ -16,4 +16,32 @@ document.addEventListener('DOMContentLoaded', function () {
     // Event listener for the mobile menu button
     let mobileMenuButton = document.getElementById('menu-button');
     mobileMenuButton.addEventListener('click', toggleMobileMenu);
+});
+
+const dropdownButton = document.getElementById('dropdown-button');
+const dropdownMenu = document.getElementById('dropdown-menu');
+let isDropdownOpen = false; // Set to true to open the dropdown by default, false to close it by default
+
+// Function to toggle the dropdown
+function toggleDropdown() {
+    isDropdownOpen = !isDropdownOpen;
+    if (isDropdownOpen) {
+        dropdownMenu.classList.remove('hidden');
+    } else {
+        dropdownMenu.classList.add('hidden');
+    }
+}
+
+// Initialize the dropdown state
+toggleDropdown();
+
+// Toggle the dropdown when the button is clicked
+dropdownButton.addEventListener('click', toggleDropdown);
+
+// Close the dropdown when clicking outside of it
+window.addEventListener('click', (event) => {
+    if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.add('hidden');
+        isDropdownOpen = false;
+    }
 });
