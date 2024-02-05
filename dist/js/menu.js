98730ca1e8d4d68e07502c76e8f8f9e384dc05e3
Aitishnitsa <aitishnitsa@gmail.com>
2024-02-05 19:37:32 +0200

    svg fix

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,61 +1,57 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Function to toggle mobile menu
-    function toggleMobileMenu() {
-        const mobileMenu = document.getElementById('mobile-menu');
-        mobileMenu.classList.toggle('hidden');
+// Function to toggle mobile menu
+function toggleMobileMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
 
+    changeButtonPath(mobileMenu);
+}
+
+function changeButtonPath(mobileMenu) {
+    const closed = document.getElementById('closed-menu');
+    const opened = document.getElementById('opened-menu');
+    const body = document.body;
+
+    closed.style.display = mobileMenu.classList.contains('hidden') ? 'block' : 'none';
+    opened.style.display = mobileMenu.classList.contains('hidden') ? 'none' : 'block';
+
+    body.classList.toggle('overflow-hidden', !mobileMenu.classList.contains('hidden'));
+}
+
+// Event listener for the mobile menu button
+document.getElementById('mobile-menu-button').addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent the click event from reaching the document
+    toggleMobileMenu();
+});
+
+// Close mobile menu when clicking outside
+document.getElementById('blur-background').addEventListener('click', (event) => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+
+    // Check if the clicked element is outside the mobile menu and button
+    if (!mobileMenu.contains(event.target) || event.target !== mobileMenuButton) {
+        mobileMenu.classList.add('hidden');
         changeButtonPath(mobileMenu);
     }
+});
 
-    function changeButtonPath(mobileMenu) {
-        const closed = document.getElementById('closed-menu');
-        const opened = document.getElementById('opened-menu');
-        const body = document.body;
+// Function to toggle dropdown menu
+function toggleDropdown(dropdownId) {
+    const dropdownMenu = document.querySelector(`#${dropdownId}`);
+    dropdownMenu.classList.toggle('hidden');
+}
 
-        closed.style.display = mobileMenu.classList.contains('hidden') ? 'block' : 'none';
-        opened.style.display = mobileMenu.classList.contains('hidden') ? 'none' : 'block';
+// Array of dropdown IDs
+const dropdownIds = ['education', 'education-mobile', 'students', 'students-mobile', 'teachers', 'teachers-mobile', 'parents', 'parents-mobile'];
 
-        body.classList.toggle('overflow-hidden', !mobileMenu.classList.contains('hidden'));
-    }
+dropdownIds.forEach(dropdownId => {
+    const dropdownButton = document.getElementById(`dropdown-${dropdownId}`);
 
-    // Event listener for the mobile menu button
-    document.getElementById('mobile-menu-button').addEventListener('click', (event) => {
+    dropdownButton.addEventListener('click', (event) => {
         event.stopPropagation(); // Prevent the click event from reaching the document
-        toggleMobileMenu();
+        toggleDropdown(`dropdown-${dropdownId}-menu`);
     });
-
-    // Close mobile menu when clicking outside
-    document.getElementById('blur-background').addEventListener('click', (event) => {
-        const mobileMenu = document.getElementById('mobile-menu');
-        const mobileMenuButton = document.getElementById('mobile-menu-button');
-
-        // Check if the clicked element is outside the mobile menu and button
-        if (!mobileMenu.contains(event.target) || event.target !== mobileMenuButton) {
-            mobileMenu.classList.add('hidden');
-            changeButtonPath(mobileMenu);
-        }
-    });
-
-    // Function to toggle dropdown menu
-    function toggleDropdown(dropdownId) {
-        const dropdownMenu = document.querySelector(`#${dropdownId}`);
-        dropdownMenu.classList.toggle('hidden');
-    }
-
-    // Array of dropdown IDs
-    const dropdownIds = ['education', 'education-mobile', 'students', 'students-mobile', 'teachers', 'teachers-mobile', 'parents', 'parents-mobile'];
-
-    // Add event listeners for the dropdown buttons dynamically
-    dropdownIds.forEach(dropdownId => {
-        const dropdownButton = document.getElementById(`dropdown-${dropdownId}`);
-
-        dropdownButton.addEventListener('click', (event) => {
-            event.stopPropagation(); // Prevent the click event from reaching the document
-            toggleDropdown(`dropdown-${dropdownId}-menu`);
-        });
-    });
-
 });
