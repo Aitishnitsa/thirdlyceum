a9e9100d45de110a141d4cf6cd1e0a1de1865d05
Aitishnitsa <aitishnitsa@gmail.com>
2024-07-20 19:30:57 +0300

    great update

---
+++ src/js/docsEmbed.js
@@ -0,0 +1,77 @@
+const container = document.getElementById('container');
+const listModeBtn = document.getElementById('list-mode');
+const gridModeBtn = document.getElementById('grid-mode');
+const loader = document.getElementById('loader');
+
+let asList = true;
+
+let list = [];
+
+const createItem = (data) => {
+    return `<a href="${data.link}">
+        <li
+            class="docs-item">
+            <div class="flex justify-center items-center w-6 sm:w-12">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 -256 1792 1792"
+                    version="1.1">
+                    <g transform="matrix(1,0,0,-1,242.98305,1285.4237)">
+                        <path
+                            d="m 1024,352 v -64 q 0,-14 -9,-23 -9,-9 -23,-9 H 288 q -14,0 -23,9 -9,9 -9,23 v 64 q 0,14 9,23 9,9 23,9 h 704 q 14,0 23,-9 9,-9 9,-23 z m 0,256 v -64 q 0,-14 -9,-23 -9,-9 -23,-9 H 288 q -14,0 -23,9 -9,9 -9,23 v 64 q 0,14 9,23 9,9 23,9 h 704 q 14,0 23,-9 9,-9 9,-23 z M 128,0 H 1152 V 768 H 736 q -40,0 -68,28 -28,28 -28,68 v 416 H 128 V 0 z m 640,896 h 299 L 768,1195 V 896 z M 1280,768 V -32 q 0,-40 -28,-68 -28,-28 -68,-28 H 96 q -40,0 -68,28 -28,28 -28,68 v 1344 q 0,40 28,68 28,28 68,28 h 544 q 40,0 88,-20 48,-20 76,-48 l 408,-408 q 28,-28 48,-76 20,-48 20,-88 z"
+                            style="fill:currentColor" />
+                    </g>
+                </svg>
+            </div>
+            <div class="sm:py-2 sm:px-4 items-center flex flex-1 overflow-hidden text-ellipsis whitespace-nowrap">
+                <p class="text-xs sm:text-base font-medium text-wrap">${data.name}</p>
+            </div>
+        </li>
+    </a>`;
+}
+
+const toggleMode = (isList) => {
+    if (isList !== asList) {
+        asList = isList;
+
+        listModeBtn.classList.toggle('border-blue', isList);
+        listModeBtn.classList.toggle('border-white', !isList);
+        gridModeBtn.classList.toggle('border-blue', !isList);
+        gridModeBtn.classList.toggle('border-white', isList);
+
+        const containerClasses = isList ? ['grid-cols-1'] : ['sm:grid-cols-3', 'grid-cols-2'];
+        container.classList.remove('grid-cols-1', 'grid-cols-2', 'sm:grid-cols-3');
+        container.classList.add(...containerClasses);
+
+        list.forEach(item => {
+            list.pop(item);
+            list.push(item);
+        });
+
+        const docsItems = document.querySelectorAll('.docs-item');
+        docsItems.forEach(item => {
+            if (isList) {
+                item.classList.remove('flex-col', 'items-center');
+            } else {
+                item.classList.add('flex-col', 'items-center');
+            }
+        });
+    }
+}
+
+listModeBtn.addEventListener('click', () => toggleMode(true));
+gridModeBtn.addEventListener('click', () => toggleMode(false));
+
+fetch('./src/docs.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(json => {
+        json.forEach(data => {
+            const item = createItem(data);
+            container.innerHTML += item;
+            list.push(item);
+        });
+        loader.classList.add('hidden');
+    });
---
+++ src/js/embed.js
@@ -0,0 +1,131 @@
+const articlesContainer = document.getElementById('articles-container');
+const readMoreButton = document.getElementById('read-more-btn');
+const showLessButton = document.getElementById('show-less-btn');
+const shadow = document.getElementById('white-shadow');
+const loader = document.getElementById('loader');
+
+let newsArray = [];
+
+// Function to create an article element
+const createArticle = (data) => {
+    const article = document.createElement('article');
+    article.className = 'bg-white border-solid border-2 border-orange hover:border-darkBlue hover:-rotate-2 transition ease-in-out delay-75 rounded-2xl p-4 flex max-w-xl flex-col items-start justify-between overflow-hidden break-words';
+
+    // Create elements for the article content
+    const dateElement = document.createElement('time');
+    dateElement.setAttribute('datetime', data.createdAt);
+    dateElement.className = 'text-blue';
+    dateElement.textContent = new Date(data.createdAt * 1000).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
+
+    const titleElement = document.createElement('p');
+    titleElement.className = 'mt-5 line-clamp-3 text-sm leading-6 text-gray-600 w-full truncate';
+
+    const titleLinkElement = document.createElement('a');
+    titleLinkElement.href = data.url;
+    titleLinkElement.innerHTML = `<p class=""></p>${data.content}`;
+
+    const authorElement = document.createElement('div');
+    authorElement.className = 'relative mt-8 flex items-center gap-x-4';
+
+    const avatarElement = document.createElement('img');
+    avatarElement.src = data.author.avatar;
+    avatarElement.alt = 'fb-avatar';
+    avatarElement.className = 'h-10 w-10 rounded-full bg-gray-50';
+
+    const authorDetailsElement = document.createElement('div');
+    authorDetailsElement.className = 'text-sm leading-6';
+
+    const authorNameElement = document.createElement('p');
+    authorNameElement.className = 'font-semibold text-gray-900';
+
+    const authorLinkElement = document.createElement('a');
+    authorLinkElement.href = data.author.profile;
+    authorLinkElement.innerHTML = `<span class="absolute inset-0"></span>${data.author.name}`;
+
+    // Append created elements to the article
+    authorNameElement.appendChild(authorLinkElement);
+    authorDetailsElement.appendChild(authorNameElement);
+    authorElement.appendChild(avatarElement);
+    authorElement.appendChild(authorDetailsElement);
+
+    titleElement.appendChild(titleLinkElement);
+
+    article.appendChild(dateElement);
+    article.appendChild(titleElement);
+    article.appendChild(authorElement);
+
+    return article;
+}
+
+// Function to render all articles on the page
+const renderAllArticles = (dataArray) => {
+    dataArray.forEach(data => {
+        const article = createArticle(data);
+        articlesContainer.appendChild(article);
+    });
+}
+
+// Function to handle "Read More" button click
+const handleReadMoreClick = () => {
+    // Show the remaining articles
+    articlesContainer.querySelectorAll('article').forEach(article => {
+        article.classList.remove('hidden');
+    });
+
+    // Hide the "Read More" button
+    readMoreButton.classList.add('hidden');
+    shadow.classList.add('hidden')
+
+    // Show the "Show Less" button
+    showLessButton.classList.remove('hidden');
+}
+
+// Function to handle "Show Less" button click
+const handleShowLessClick = () => {
+    // Hide the remaining articles
+    articlesContainer.querySelectorAll('article').forEach((article, index) => {
+        if (index >= 6) {
+            article.classList.add('hidden');
+        }
+    });
+
+    // Show the "Read More" button
+    readMoreButton.classList.remove('hidden');
+    shadow.classList.remove('hidden')
+
+    // Hide the "Show Less" button
+    showLessButton.classList.add('hidden');
+}
+
+// Initial articles rendering
+fetch('../src/posts.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(json => {
+        // Render all articles
+        renderAllArticles(json);
+
+        // Show only 6 fully visible articles and hide the rest
+        articlesContainer.querySelectorAll('article').forEach((article, index) => {
+            if (index >= 6) {
+                article.classList.add('hidden');
+            }
+        });
+
+        // Show the "Read More" button if there are more articles
+        if (json.length > 9) {
+            readMoreButton.classList.remove('hidden');
+        }
+
+        loader.classList.add('hidden');
+    });
+
+// Event listener for the "Read More" button
+readMoreButton.addEventListener('click', handleReadMoreClick);
+
+// Event listener for the "Show Less" button
+showLessButton.addEventListener('click', handleShowLessClick);
